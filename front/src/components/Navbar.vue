70e1d6fe4a264ab9729e1cbc8dd36bc34d002be8
Davi ferreira de sena <davifsena2@gmail.com>
2022-08-12 12:16:52 -0300

    Criando layout da pagina principal

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <nav id="navbar">
+
+    </nav>
+
+</template>
+
+<script>
+
+export default {
+
+    name: "Navbar"
+
+}
+
+</script>
+
+<style scoped>
+
+    #navbar{
+        background-color: #FFFFFF;
+        width: 100vw;
+        height: 50px;
+    }
+
+</style>
